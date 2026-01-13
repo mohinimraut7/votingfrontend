c9dd6652d0ea70542b085ca356f0fd351ebd85bf
mohinimraut7 <mohinimraut7@gmail.com>
2026-01-13 14:11:13 +0530

    build removed

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 // export const baseUrl='http://localhost:5000/api';
 // export const billBaseUrl = 'http://localhost:5000';
-// export const fileBaseUrl = 'http://localhost:5000';
+// export const fileBaseUrl = 'http://localhost:5000' ;
 
 export const baseUrl='https://votingserver-z9ic.onrender.com/api';
 export const billBaseUrl = 'https://votingserver-z9ic.onrender.com';
