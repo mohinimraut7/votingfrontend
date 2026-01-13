97ed253638ef2eb9407113136ee42f2b6aa0665d
mohinimraut7 <mohinimraut7@gmail.com>
2026-01-13 19:05:13 +0530

    slip android changes

--- src/config/config.js
+++ src/config/config.js
@@ -4,12 +4,12 @@
 
 // https://voting.saavi.co.in/api
 
-// export const baseUrl='https://votingserver-z9ic.onrender.com/api';
-// export const billBaseUrl = 'https://votingserver-z9ic.onrender.com';
-// export const fileBaseUrl = 'https://votingserver-z9ic.onrender.com';
+export const baseUrl='https://votingserver-z9ic.onrender.com/api';
+export const billBaseUrl = 'https://votingserver-z9ic.onrender.com';
+export const fileBaseUrl = 'https://votingserver-z9ic.onrender.com';
 
 
-export const baseUrl='https://voting.saavi.co.in/api';
-export const billBaseUrl = 'https://voting.saavi.co.in/api';
-export const fileBaseUrl = 'https://voting.saavi.co.in/api';
+// export const baseUrl='https://voting.saavi.co.in/api';
+// export const billBaseUrl = 'https://voting.saavi.co.in/api';
+// export const fileBaseUrl = 'https://voting.saavi.co.in/api';
 
