59bc27cfb26ccc373abe5fff262c5bd7910302f9
mohinimraut7 <mohinimraut7@gmail.com>
2026-01-13 14:09:31 +0530

    config changes url chnged

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,8 @@
-export const baseUrl='http://localhost:5000/api';
-export const billBaseUrl = 'http://localhost:5000';
-export const fileBaseUrl = 'http://localhost:5000';
+// export const baseUrl='http://localhost:5000/api';
+// export const billBaseUrl = 'http://localhost:5000';
+// export const fileBaseUrl = 'http://localhost:5000';
 
-// export const baseUrl='https://tenderserver.onrender.com/api';
-// export const billBaseUrl = 'https://tenderserver.onrender.com';
-// export const fileBaseUrl = 'https://tenderserver.onrender.com';
+export const baseUrl='https://votingserver-z9ic.onrender.com/api';
+export const billBaseUrl = 'https://votingserver-z9ic.onrender.com';
+export const fileBaseUrl = 'https://votingserver-z9ic.onrender.com';
+
