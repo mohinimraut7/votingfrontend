7e28cd70930ca5ab07e97a128b738c080d771e69
mohinimraut7 <mohinimraut7@gmail.com>
2026-01-14 23:18:03 +0530

    live URL changed

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
-export const baseUrl='http://localhost:5000/api';
-export const billBaseUrl = 'http://localhost:5000';
-export const fileBaseUrl = 'http://localhost:5000' ;
+// export const baseUrl='http://localhost:5000/api';
+// export const billBaseUrl = 'http://localhost:5000';
+// export const fileBaseUrl = 'http://localhost:5000' ;
 
 
 
@@ -9,7 +9,7 @@ export const fileBaseUrl = 'http://localhost:5000' ;
 // export const fileBaseUrl = 'https://votingserver-z9ic.onrender.com';
 
 
-// export const baseUrl='https://voting.saavi.co.in/api';
-// export const billBaseUrl = 'https://voting.saavi.co.in/api';
-// export const fileBaseUrl = 'https://voting.saavi.co.in/api';
+export const baseUrl='https://voting.saavi.co.in/api';
+export const billBaseUrl = 'https://voting.saavi.co.in/api';
+export const fileBaseUrl = 'https://voting.saavi.co.in/api';
 
